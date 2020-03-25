abd2a4e3e7caeb247c84f3402be26c1df042ad8f
QingpingMeng <missing1989@gmail.com>
2020-03-24 20:24:01 -0700

    show daily trend

--- src/components/Charts/ExpenseTrend.vue
+++ src/components/Charts/ExpenseTrend.vue
@@ -4,23 +4,28 @@
 
 <script lang="ts">
 import { Component, Vue, Watch } from "vue-property-decorator";
-import { State } from "vuex-class";
+import { Getter } from "vuex-class";
 import Chart from "chart.js";
+import dayjs from "dayjs";
+import Transaction from "@/models/transaction";
 @Component
 export default class ExpenseTrend extends Vue {
+  @Getter("orderedTransactions") private localTransactions!: Transaction[];
+  @Getter transactionDates!: string[];
+
   private lineChart!: Chart;
-  private weelyData = [0, 0, 0, 0, 0, 0, 0];
+  private dayRange = this.getDaysRange();
 
   public mounted() {
-    var ctx = this.$refs.canvas as HTMLCanvasElement;
-    var lineChart = new Chart(ctx, {
+    const ctx = this.$refs.canvas as HTMLCanvasElement;
+    this.lineChart = new Chart(ctx, {
       type: "line",
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: this.dayRange.map(d => d.format("MM/DD")),
         datasets: [
           {
             label: "Daily Expense",
-            data: this.weelyData,
+            data: this.getExpenseData(),
             borderWidth: 2
           }
         ]
@@ -36,6 +41,45 @@ export default class ExpenseTrend extends Vue {
             }
           ]
         }
+      }
+    });
+  }
+
+  private getDaysRange() {
+    const now = dayjs();
+    const days = [];
+    for (let i = 4; i > 0; i--) {
+      days.push(now.subtract(i, "day"));
+    }
+    days.push(now);
+    for (let i = 1; i <= 4; i++) {
+      days.push(now.add(i, "day"));
+    }
+    return days;
+  }
+
+  private getExpenseData() {
+    const startTimeStamp = this.dayRange[0].valueOf();
+    const endTimeStamp = this.dayRange
+      .slice(-1)
+      .pop()!
+      .add(1, "day")
+      .valueOf();
+    let transactionsInRange = this.localTransactions.filter(
+      t => t.timestamp < endTimeStamp && t.timestamp >= startTimeStamp
+    );
+
+    return this.dayRange.map(day => {
+      if (
+        !this.transactionDates.find(date => date === day.format("YYYY-MM-DD"))
+      ) {
+        return 0;
+      } else {
+        let date = day.format("YYYY-MM-DD");
+        let totalAmountInDay = transactionsInRange
+          .filter(t => t.dateString === date)
+          .reduce((sum, currentT) => sum + currentT.amount, 0);
+        return totalAmountInDay;
       }
     });
   }
